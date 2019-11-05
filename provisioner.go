75467cdb4630f93f58c1655c0986dc77e04522da
ahmadali shafiee <mail@ahmadalli.net>
2019-11-05 18:29:25 +0330

    remove node affinity

--- provisioner.go
+++ provisioner.go
@@ -212,23 +212,6 @@ func (p *LocalPathProvisioner) Provision(opts pvController.VolumeOptions) (*v1.P
 					Type: &hostPathType,
 				},
 			},
-			NodeAffinity: &v1.VolumeNodeAffinity{
-				Required: &v1.NodeSelector{
-					NodeSelectorTerms: []v1.NodeSelectorTerm{
-						{
-							MatchExpressions: []v1.NodeSelectorRequirement{
-								{
-									Key:      KeyNode,
-									Operator: v1.NodeSelectorOpIn,
-									Values: []string{
-										node.Name,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
 		},
 	}, nil
 }
